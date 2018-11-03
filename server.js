e53567cecf65fe8d2f8662d9d96b0ac155f39cb2
Kenny <kennethpolyak@gmail.com>
2018-11-02 20:42:33 -0400

    Set up proxy for test

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const path = require('path');
 const app = express();
 const port = process.env.PORT || 4000;
+const axios = require('axios')
 
 const proxy = express();
 proxy.use(morgan('dev'));
@@ -14,6 +15,20 @@ proxy.listen(port, function() {
 });
 
 proxy.use(express.static(path.join(__dirname, 'public')));
+
+proxy.get('/rooms/:id', function(req, res) {
+  const id = req.params.id;``
+  axios
+    .get(`http://18.219.227.74/listings/${id}`)
+    .then((response) => {
+      res.send(response.data)
+    })
+    .catch(e => console.log('there is an error!', e));
+});
+
+proxy.get('/test', (req, res) => {
+  res.send('TEST')
+})
 
 proxy.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
