a473aab143c87a214f7fb8e5de53f3e2d7eef2bf
Kenny <kennethpolyak@gmail.com>
2018-11-03 10:24:32 -0400

    Add gallery and related listings

--- server.js
+++ server.js
@@ -15,38 +15,38 @@ app.use(cors())
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Booker API Routes
-// app.get('/listings/:id', function(req, res) {
-//   const id = req.params.id;
-//   axios
-//     .get(`http://18.219.227.74/listings/${id}`)
-//     .then((response) => {
-//       res.send(response.data)
-//     })
-//     .catch(e => console.log('there is an error!', e));
-// });
+app.get('/listings/:id', function(req, res) {
+  const id = req.params.id;
+  axios
+    .get(`http://18.219.227.74/listings/${id}`)
+    .then((response) => {
+      res.send(response.data)
+    })
+    .catch(e => console.log('there is an error!', e));
+});
 
-// app.get('/unavailabilities/:id', function(req, res) {
-//   const id = req.params.id;
-//   axios
-//     .get(`http://18.219.227.74/unavailabilities/${id}`)
-//     .then((response) => {
-//       res.send(response.data)
-//     })
-//     .catch(e => console.log('there is an error!', e));
-// });
+app.get('/unavailabilities/:id', function(req, res) {
+  const id = req.params.id;
+  axios
+    .get(`http://18.219.227.74/unavailabilities/${id}`)
+    .then((response) => {
+      res.send(response.data)
+    })
+    .catch(e => console.log('there is an error!', e));
+});
 
-// app.post('/bookings/', function(req, res) {
-//   axios
-//     .get(`http://18.219.227.74/bookings/`)
-//     .then((response) => {
-//       res.send(response.data)
-//     })
-//     .catch(e => console.log('there is an error!', e));
-// });
+app.post('/bookings/', function(req, res) {
+  axios
+    .get(`http://18.219.227.74/bookings/`)
+    .then((response) => {
+      res.send(response.data)
+    })
+    .catch(e => console.log('there is an error!', e));
+});
 
-// app.get('/*', (req, res) => {
-//   res.sendFile(path.join(__dirname, 'public/index.html'))
-// })
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public/index.html'))
+})
 
 // app.get('/test', (req, res) => {
 //   res.send('TEST')
